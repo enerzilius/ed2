51019ed019d3b82c1861830cd0d2834c16c68806
enerzilius <efblouback@gmail.com>
2025-09-11 16:52:29 -0300

    coisa do professor

---
+++ ordenacao/selection.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include "utils.cpp"
+
+using namespace std;
+
+
+template<typename T>
+int pos_maior(T *v, int ini, int fim) {
+    int maior = ini;
+    for(int i = ini+1; i <= fim; i++) {
+        if(v[i] > v[maior]) maior = i;
+    }
+    return maior;
+}
+
+void selectionSort(int *v, int n) {
+    for (int i = n-1; i > 0; i--)
+    {
+        int maior = pos_maior<int>(v, 0, i);
+        trocar(v, maior, i);
+    }
+}
+
+
+int main() {
+    int n =10;
+
+    vector<int> v;
+    v = gerar_vetor_aleatorio(n, 99, n*100);
+
+    imprimir_vetor(v.data(), v.size());
+    selectionSort(v.data(), v.size());
+
+    imprimir_vetor(v.data(), v.size());
+
+    return 0;
+}
