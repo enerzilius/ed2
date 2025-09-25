d5868f48d71b8f7587238df4cb0999d5d3ed36c2
enerzilius <efblouback@gmail.com>
2025-09-25 16:06:56 -0300

    antes de começar o quicksort

---
+++ ordenacao/quick.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include "utils.cpp"
+
+using namespace std;
+
+// PARTICIONAMENTO
+
+
+int main(int argc, char** argv) {
+    return 0;
+}
