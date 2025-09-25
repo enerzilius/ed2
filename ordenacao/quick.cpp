05dac98b5121af5eb1028e00f2bbc65e0151215d
enerzilius <efblouback@gmail.com>
2025-09-25 16:22:29 -0300

    primeiros comentários da teoria

--- ordenacao/quick.cpp
+++ ordenacao/quick.cpp
@@ -7,6 +7,16 @@ using namespace std;
 
 // PARTICIONAMENTO
 
+template<typename T>
+void quickSort() {
+    // p -> primeiro
+    // r -> último (pivô)
+    // pivô (último elemento) -> divide em duas partições, uma com valores só menores que ele e outra com maiores
+
+
+    return;
+}
+
 
 int main(int argc, char** argv) {
     return 0;
