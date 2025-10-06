355c254cd74226aafa77aca2bf4faf70f5855b4d
enerzilius <efblouback@gmail.com>
2025-10-06 19:57:43 -0300

    começando hashing

---
+++ dicionarios/hashing.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <unordered_set>
+#include <utility>
+#include "../utils/utils.h"
+
+using namespace std;
+
+template<typename T>
+void hashing(T x, int M) {
+    return x % M;
+}
+
+
+int main(int argc, char** argv) {
+    return 0;
+}
