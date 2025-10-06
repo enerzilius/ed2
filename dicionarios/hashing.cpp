52c1a7812c10225d805e8a80b1ce8ce67a87d00f
enerzilius <efblouback@gmail.com>
2025-10-06 20:23:02 -0300

    encademento

--- dicionarios/hashing.cpp
+++ dicionarios/hashing.cpp
@@ -7,8 +7,20 @@
 
 using namespace std;
 
+//Minimiza o número de colisões
+//Espalha uniformimente as chaves pelas posições da tabela
+    //Todas as posições da tabela sejam igualmente prováveis de receberam chaves
+
+//Tratamento de colisões por encadeamento
+//Encadeia um par chave e valor com mesmo módulo
+/*
+h(999) = 999 % 10 = 9
+h(99) = 99 %10 = 9
+9 |  |->(999,1);(99,1);
+*/
 template<typename T>
 void hashing(T x, int M) {
+    
     return x % M;
 }
 
